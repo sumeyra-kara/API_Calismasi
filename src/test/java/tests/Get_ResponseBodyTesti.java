91bdbda8c963522d14f4415017cf402356c77eef
karasumeyra <sumeyrakara93@gmail.com>
2024-07-08 20:43:01 +0200

    Json Object und Matches

---
+++ src/test/java/tests/Get_ResponseBodyTesti.java
@@ -0,0 +1,52 @@
+package tests;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+
+import static io.restassured.RestAssured.given;
+
+public class Get_ResponseBodyTesti {
+    /*
+          https://jsonplaceholder.typicode.com/posts/44 url'ine bir GET request yolladigimizda donen Response’in
+      status code'unun 200,
+      ve content type'inin Aplication.JSON,
+      ve response body'sinde bulunan userId'nin 5,
+      ve response body'sinde bulunan title'in "optio dolor molestias sit"
+      oldugunu test edin.
+           */
+    @Test
+    public void get1(){
+        // 1- Endpoint hazirla
+        String url = "https://jsonplaceholder.typicode.com/posts/44";
+
+        // 2- Expected Data hazirla
+        // 3- Response'i kaydet
+        Response response = given().when().get(url);
+        response.prettyPrint();
+
+        // 4- Aseert
+
+        response.then().assertThat()
+                    .statusCode(200)
+                    .contentType(ContentType.JSON)
+                    .body("userId", Matchers.equalTo(5))
+                    .body("title", Matchers.equalTo("optio dolor molestias sit"));
+    }
+
+    /*
+    ************+ aciklama ****************
+    Response body’sindeki degerleri test etmek icin Matchers class’indan yardim aliriz.
+        response.
+        then().
+        assertThat().
+        body("title",Matchers.equalTo("optio dolor molestias sit")).
+        body("userId",Matchers.equalTo(5));
+     */
+
+
+
+}
---
+++ src/test/java/tests/JsonObjesiOlusturma.java
@@ -0,0 +1,61 @@
+package tests;
+
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class JsonObjesiOlusturma {
+    /*
+    Asagidaki JSON Objesini olusturup konsolda yazdirin.
+    {
+    "title":"Ahmet",
+    "body":"Merhaba",
+    "userId":1
+    }
+     */
+
+    @Test
+    public void jsonObje01(){
+
+        JSONObject ilkJsonObject = new JSONObject();
+        ilkJsonObject.put("title","Ahmet");
+        ilkJsonObject.put("body","Merhaba");
+        ilkJsonObject.put("userId",1);
+
+        System.out.println("ilkJsonObject = " + ilkJsonObject); // ilkJsonObject = {"title":"Ahmet","body":"Merhaba","userId":1}
+    }
+
+    @Test
+    public void jsonObje2(){
+        /*
+     Asagidaki JSON Objesini olusturup konsolda yazdirin.
+    {
+    "firstname":"Jim",
+    "additionalneeds":"Breakfast",
+    "bookingdates":{
+    "checkin":"2018-01-01",
+    "checkout":"2019-01-01"
+    },
+    "totalprice":111,
+    "depositpaid":true,
+    "lastname":"Brown"
+    }
+    */
+
+     JSONObject bookingDates = new JSONObject();
+     bookingDates.put("checkin","2018-01-01");
+     bookingDates.put("checkout","2019-01-01");
+
+    JSONObject outerJson  =new JSONObject();
+    outerJson.put("firstname","Jim");
+    outerJson.put("additionalneeds","Breakfast");
+    outerJson.put("totalprice",111);
+    outerJson.put("depositpaid",true);
+    outerJson.put("lastname","Brown");
+    outerJson.put("bookingdates",bookingDates);
+
+    System.out.println("outerJson = " + outerJson);
+
+    }
+
+
+}
