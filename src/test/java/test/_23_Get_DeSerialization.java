e8fc7d8a6b05b4e43465bed7d256e94e867e6658
karasumeyra <sumeyrakara93@gmail.com>
2024-07-10 22:34:18 +0200

    Serialization and de-Serialization

--- src/test/java/test/_23_Get_DeSerialization.java
+++ src/test/java/test/_23_Get_DeSerialization.java
@@ -16,6 +16,12 @@ import static org.junit.Assert.assertEquals;
 
 public class _23_Get_DeSerialization extends BaseUrlDummyExample {
     /**
+     * işlem yaptığımız nesneyi, sınıfı saklamak yada transfer etmek istediğimiz formata
+     * dönüştürme işlemine Serialization denir.
+     * Verilen Json objesini testlerimizde kullanmak uzere Java objesine cevirmeye ise De-Serialization denir
+     */
+
+    /**
      * http://dummy.restapiexample.com/api/v1/employee/3 url’ine bir GET request
      * gonderdigimizde donen response’un status code’unun 200, content Type’inin
      * application/json ve body’sinin asagidaki gibi oldugunu test edin.
@@ -36,42 +42,36 @@ public class _23_Get_DeSerialization extends BaseUrlDummyExample {
     public void test01(){
         // 1- endpoint ve request body olustur
         specDummyExample.pathParams("pp1","employee","pp2","3");
+
         // 2- Soruda varsa expected data olustur
         Map<String,Object> expectedData = TestDataDummyExample.mapBodyOlustur();
 
         // 3- Request gonder donen response'i kaydet
-
         Response response= given().spec(specDummyExample)
                 .when()
                 .get("{pp1}/{pp2}");
-        Map<String,Object> responseMap= response.as(HashMap.class);
+        Map<String,Object> responseMap= response.as(Map.class);
+        // response objesini De-Serialization ile Map’e cevirmek icin Gson kutuphanesi gerekli
 
         // 4- Assertion
+        assertEquals(TestDataDummyExample.basariliSorguStatusCode,response.statusCode());// donen response’un status code’unun 200,
+        assertEquals(TestDataDummyExample.contentType,response.contentType());// content Type’inin application/json
 
-        // donen response’un status code’unun 200,
-        assertEquals(TestDataDummyExample.basariliSorguStatusCode,response.statusCode());
-        // content Type’inin application/json
-        assertEquals(TestDataDummyExample.contentType,response.contentType());
         // ve body’sinin asagidaki gibi oldugunu test edin.
-
         assertEquals(expectedData.get("message"),responseMap.get("message"));
         assertEquals(expectedData.get("status"),responseMap.get("status"));
 
-        assertEquals(((Map)expectedData.get("data")).get("profile_image"),
-                ((Map)responseMap.get("data")).get("profile_image"));
+        // map icerisinde map oldugu icin cating yapmak gerekiyor
+        assertEquals(((Map)expectedData.get("data")).get("profile_image"),((Map)responseMap.get("data")).get("profile_image"));
 
-        assertEquals(((Map) expectedData.get("data")).get("employee_name"),
-                ((Map) responseMap.get("data")).get("employee_name"));
+        assertEquals(((Map) expectedData.get("data")).get("employee_name"),((Map) responseMap.get("data")).get("employee_name"));
 
-        assertEquals(((Map) expectedData.get("data")).get("employee_salary"),
-                ((Map) responseMap.get("data")).get("employee_salary"));
+        assertEquals(((Map) expectedData.get("data")).get("employee_salary"),((Map) responseMap.get("data")).get("employee_salary"));
 
+        assertEquals(((Map) expectedData.get("data")).get("id"),((Map) responseMap.get("data")).get("id"));
 
-        assertEquals(((Map) expectedData.get("data")).get("id"),
-                ((Map) responseMap.get("data")).get("id"));
+        assertEquals(((Map) expectedData.get("data")).get("employee_age"),((Map) responseMap.get("data")).get("employee_age"));
 
-        assertEquals(((Map) expectedData.get("data")).get("employee_age"),
-                ((Map) responseMap.get("data")).get("employee_age"));
 
     }
 }
--- src/test/java/test/_24_Post_Deserialization.java
+++ src/test/java/test/_24_Post_Deserialization.java
@@ -49,45 +49,34 @@ public class _24_Post_Deserialization extends HerokuappBaseUrl {
     @Test
     public void test01(){
         // 1- endpoint ve request body olustur
-
         specHerokuApp.pathParam("pp1","booking");
-
         Map<String,Object> requestBodyMap= TestDataHerokuapp.requestBodyMapOlustur();
 
         // 2- Soruda varsa expected data olustur
-
         Map<String,Object> expectedData= TestDataHerokuapp.responseBodyMapOlustur();
 
         // 3- Request gonder donen response'i kaydet
-
         Response response= given().contentType(ContentType.JSON).spec(specHerokuApp)
                 .when().body(requestBodyMap)
                 .post("{pp1}");
-
         Map<String,Object> responseMap= response.as(HashMap.class);
 
         // 4- Assertion
+        assertEquals(((Map)expectedData.get("booking")).get("firstname"),((Map)responseMap.get("booking")).get("firstname"));
 
-        assertEquals(((Map)expectedData.get("booking")).get("firstname"),
-                ((Map)responseMap.get("booking")).get("firstname"));
+        assertEquals(((Map) expectedData.get("booking")).get("lastname"),((Map) responseMap.get("booking")).get("lastname"));
 
-        assertEquals(((Map) expectedData.get("booking")).get("lastname"),
-                ((Map) responseMap.get("booking")).get("lastname"));
+        assertEquals(((Map) expectedData.get("booking")).get("additionalneeds"),((Map) responseMap.get("booking")).get("additionalneeds"));
 
-        assertEquals(((Map) expectedData.get("booking")).get("additionalneeds"),
-                ((Map) responseMap.get("booking")).get("additionalneeds"));
+        assertEquals(((Map) expectedData.get("booking")).get("totalprice"),((Map) responseMap.get("booking")).get("totalprice"));
 
-        assertEquals(((Map) expectedData.get("booking")).get("totalprice"),
-                ((Map) responseMap.get("booking")).get("totalprice"));
-
-        assertEquals(((Map) expectedData.get("booking")).get("depositpaid"),
-                ((Map) responseMap.get("booking")).get("depositpaid"));
+        assertEquals(((Map) expectedData.get("booking")).get("depositpaid"),((Map) responseMap.get("booking")).get("depositpaid"));
 
         assertEquals(((Map)((Map) expectedData.get("booking")).get("bookingdates")).get("checkin"),
-                ((Map)((Map) expectedData.get("booking")).get("bookingdates")).get("checkin"));
+                    ((Map)((Map) expectedData.get("booking")).get("bookingdates")).get("checkin"));
 
         assertEquals(((Map)((Map) expectedData.get("booking")).get("bookingdates")).get("checkout"),
-                ((Map)((Map) expectedData.get("booking")).get("bookingdates")).get("checkout"));
+                     ((Map)((Map) expectedData.get("booking")).get("bookingdates")).get("checkout"));
 
 
     }
